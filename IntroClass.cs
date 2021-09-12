2ee023cc2141e85613f096b3408ee78c1fc61802
Elango Sengottaiyan <elango.sengottaiyan@outlook.com>
2021-09-12 18:15:00 +0530

    Add project files.

---
+++ IntroClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P_Oops
+{
+    /// <summary>
+    /// Class 
+    /// * Default access modifier of the class is Internal
+    /// * Default access modifier of the Member is Private
+    /// </summary>
+    class IntroClass
+    {
+        #region "Declaration and Initialization"
+
+        #endregion
+
+        #region "Methods"
+        /// <summary>
+        /// Basic call between the classes
+        /// </summary>
+        public void BasicExecution()
+        {
+            Console.WriteLine("Welcome");
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P_Oops
+{
+    class Program
+    {
+        #region "Declaration and Initialization"
+        static IntroClass Class = new IntroClass();
+        #endregion
+
+        #region "Methods"
+        static void Main(string[] args)
+        {
+            Class.BasicExecution();
+        }
+        #endregion
+    }
+}
